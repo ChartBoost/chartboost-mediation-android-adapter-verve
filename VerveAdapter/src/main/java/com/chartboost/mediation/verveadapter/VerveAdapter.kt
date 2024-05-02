622a46e795c95a8b07da5e5581aea3939eeb72be
vu <vu.chau@chartboost.com>
2024-05-02 17:47:52 -0400

    AdapterConfiguration

--- VerveAdapter/src/main/java/com/chartboost/mediation/verveadapter/VerveAdapter.kt
+++ VerveAdapter/src/main/java/com/chartboost/mediation/verveadapter/VerveAdapter.kt
@@ -28,7 +28,6 @@ import net.pubnative.lite.sdk.interstitial.HyBidInterstitialAd
 import net.pubnative.lite.sdk.models.AdSize
 import net.pubnative.lite.sdk.models.ImpressionTrackingMethod
 import net.pubnative.lite.sdk.rewarded.HyBidRewardedAd
-import net.pubnative.lite.sdk.utils.Logger
 import net.pubnative.lite.sdk.views.HyBidAdView
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
@@ -40,33 +39,15 @@ import kotlin.coroutines.suspendCoroutine
 class VerveAdapter : PartnerAdapter {
     companion object {
         /**
-         * Test mode option that can be enabled to test Verve SDK integrations.
-         */
-        var testModeEnabled = HyBid.isTestMode()
-            set(value) {
-                HyBid.setTestMode(value)
-                field = value
-                PartnerLogController.log(
-                    CUSTOM,
-                    "Verve SDK test mode is ${if (value) "enabled" else "disabled"}.",
-                )
-            }
-
-        /**
-         * Log level option that can be set to alter the output verbosity.
-         */
-        var logLevel = Logger.Level.info
-            set(value) {
-                HyBid.setLogLevel(value)
-                field = value
-                PartnerLogController.log(CUSTOM, "Verve SDK log level set to $value.")
-            }
-
-        /**
          * Key for parsing the Verve SDK app token.
          */
         private const val APP_TOKEN_KEY = "app_token"
     }
+
+    /**
+     * The Verve adapter configuration.
+     */
+    override var configuration: PartnerAdapterConfiguration = VerveAdapterConfiguration
 
     /**
      * A map of Chartboost Mediation load identifiers with their corresponding Verve HyBidAdInterstitial ads.
@@ -77,39 +58,6 @@ class VerveAdapter : PartnerAdapter {
      * A map of Chartboost Mediation load identifiers with their corresponding Verve HyBidRewardedAd ads.
      */
     private val loadIdToHyBidRewardedAds = mutableMapOf<String, HyBidRewardedAd>()
-
-    /**
-     * Get the Verve SDK version.
-     */
-    override val partnerSdkVersion: String
-        get() = HyBid.getHyBidVersion()
-
-    /**
-     * Get the Verve adapter version.
-     *
-     * You may version the adapter using any preferred convention, but it is recommended to apply the
-     * following format if the adapter will be published by Chartboost Mediation:
-     *
-     * Chartboost Mediation.Partner.Adapter
-     *
-     * "Chartboost Mediation" represents the Chartboost Mediation SDK’s major version that is compatible with this adapter. This must be 1 digit.
-     * "Partner" represents the partner SDK’s major.minor.patch.x (where x is optional) version that is compatible with this adapter. This can be 3-4 digits.
-     * "Adapter" represents this adapter’s version (starting with 0), which resets to 0 when the partner SDK’s version changes. This must be 1 digit.
-     */
-    override val adapterVersion: String
-        get() = BuildConfig.CHARTBOOST_MEDIATION_VERVE_ADAPTER_VERSION
-
-    /**
-     * Get the partner name for internal uses.
-     */
-    override val partnerId: String
-        get() = "verve"
-
-    /**
-     * Get the partner name for external uses.
-     */
-    override val partnerDisplayName: String
-        get() = "Verve"
 
     /**
      * Initialize the Verve SDK so that it is ready to request ads.
---
+++ VerveAdapter/src/main/java/com/chartboost/mediation/verveadapter/VerveAdapterConfiguration.kt
@@ -0,0 +1,60 @@
+package com.chartboost.mediation.verveadapter
+
+import com.chartboost.chartboostmediationsdk.domain.PartnerAdapterConfiguration
+import com.chartboost.chartboostmediationsdk.utils.PartnerLogController
+import net.pubnative.lite.sdk.HyBid
+import net.pubnative.lite.sdk.utils.Logger
+
+object VerveAdapterConfiguration : PartnerAdapterConfiguration {
+    /**
+     * The partner name for internal uses.
+     */
+    override val partnerId = "verve"
+
+    /**
+     * The partner name for external uses.
+     */
+    override val partnerDisplayName = "Verve"
+
+    /**
+     * The partner SDK version.
+     */
+    override val partnerSdkVersion: String = HyBid.getHyBidVersion()
+
+    /**
+     * The partner adapter version.
+     *
+     * You may version the adapter using any preferred convention, but it is recommended to apply the
+     * following format if the adapter will be published by Chartboost Mediation:
+     *
+     * Chartboost Mediation.Partner.Adapter
+     *
+     * "Chartboost Mediation" represents the Chartboost Mediation SDK’s major version that is compatible with this adapter. This must be 1 digit.
+     * "Partner" represents the partner SDK’s major.minor.patch.x (where x is optional) version that is compatible with this adapter. This can be 3-4 digits.
+     * "Adapter" represents this adapter’s version (starting with 0), which resets to 0 when the partner SDK’s version changes. This must be 1 digit.
+     */
+    override val adapterVersion = BuildConfig.CHARTBOOST_MEDIATION_VERVE_ADAPTER_VERSION
+
+    /**
+     * Test mode option that can be enabled to test Verve SDK integrations.
+     */
+    var testModeEnabled = HyBid.isTestMode()
+        set(value) {
+            HyBid.setTestMode(value)
+            field = value
+            PartnerLogController.log(
+                    PartnerLogController.PartnerAdapterEvents.CUSTOM,
+                    "Verve SDK test mode is ${if (value) "enabled" else "disabled"}.",
+            )
+        }
+
+    /**
+     * Log level option that can be set to alter the output verbosity.
+     */
+    var logLevel = Logger.Level.info
+        set(value) {
+            HyBid.setLogLevel(value)
+            field = value
+            PartnerLogController.log(PartnerLogController.PartnerAdapterEvents.CUSTOM, "Verve SDK log level set to $value.")
+        }
+}
