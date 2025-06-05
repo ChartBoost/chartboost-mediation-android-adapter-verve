babc3ed1a8a71552b0983ad49482a07da184448c
Helium Service <helium-service@chartboost.com>
2025-06-05 00:14:25 +0000

    5.3.6.0.0

--- VerveAdapter/build.gradle.kts
+++ VerveAdapter/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         minSdk = 21
         targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.3.2.1.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.3.6.0.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_VERVE_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
@@ -92,7 +92,7 @@ dependencies {
     "candidateImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Partner SDK
-    implementation("net.pubnative:hybid.sdk:3.2.1")
+    implementation("net.pubnative:hybid.sdk:3.6.0")
 
     // Adapter Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
